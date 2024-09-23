8c71bf29e00bce6dd275dc5da51b57463a310630
Breanna Burris <breanna1burris@gmail.com>
2024-09-23 17:22:25 -0400

    Createcompany structure with departments employees

---
+++ department_salary.js
@@ -0,0 +1,55 @@
+// Task 1: Create a Department Structure
+const company = { //object to create department structure
+    departments: [
+        {
+            departmentName: 'Engineering',
+            employees: [
+                {
+                    name: 'Alice',
+                    salary: 100000,
+                    subordinates: [
+                        {
+                            name: 'Bob',
+                            salary: 80000,
+                            subordinates: [
+                                {
+                                    name: 'Charlie',
+                                    salary: 60000,
+                                    subordinates: []
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                    name: 'David',
+                    salary: 90000,
+                    subordinates: []
+                    }   
+                ]   
+            },
+        {
+            departmentName: 'Business Intelligence',
+            employees: [
+                {
+                    name: 'Eve',
+                    salary: 85000,
+                    subordinates: [
+                        {
+                            name: 'Frank',
+                            salary: 70000,
+                            subordinates: []
+                        }
+                    ]
+                },
+                {
+                    name: 'Grace',
+                    salary: 95000,
+                    subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
+console.log(company);
